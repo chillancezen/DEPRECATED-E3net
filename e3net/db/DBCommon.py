f98435ac5c56927a3b50d17a19b9d51a5f90a568
Linc <chillancezen@gmail.com>
2017-06-28 20:28:21 +0800

    central authentication service with role and tenant features finished

---
+++ e3net/db/DBCommon.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+from sqlalchemy import *
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import uuid
+
+E3DBBase=declarative_base()
+E3DBSession=sessionmaker(autocommit=True)
+E3DBEngine=None
+
+def create_database_entries(engine=None):
+	if engine == None:
+		engine = E3DBEngine
+	E3DBBase.metadata.create_all(engine)
+
+def bind_session_with_engine(engine=None):
+	if engine == None:
+		engine=E3DBEngine
+	E3DBSession.configure(bind=engine)
+def init_e3_database(conn,echo=False):
+	global E3DBEngine
+	E3DBEngine=create_engine(conn,echo=echo)
+	bind_session_with_engine(E3DBEngine)
---
+++ e3net/db/DBE3token.py
@@ -0,0 +1,227 @@
+#! /usr/bin/python
+from e3net.db.DBCommon import *
+from uuid import uuid4
+import json
+
+class Role(E3DBBase):
+	__tablename__='role'
+	
+	id=Column(String(64),primary_key=True)
+	name=Column(String(64),nullable=False,index=True,unique=True)
+	description=Column(Text,nullable=True)
+
+	def __repr__(self):
+		obj=dict()
+		obj['id']=self.id
+		obj['name']=self.name
+		obj['description']=self.description
+		return str(obj)
+	
+class Tenant(E3DBBase):
+	__tablename__='tenant'
+	
+	id=Column(String(64),primary_key=True)
+	name=Column(String(64),nullable=False,index=True,unique=True)
+	passwd=Column(String(64),nullable=False)
+	description=Column(Text,nullable=True)
+	enabled=Column(Boolean,nullable=False)
+	role_id=Column(String(64),ForeignKey('role.id'))
+	
+	def __repr__(self):
+		obj=dict()
+		obj['id']=self.id
+		obj['name']=self.name
+		obj['passwd']=self.passwd
+		obj['description']=self.description
+		obj['enabled']=self.enabled
+		obj['role_id']=self.role_id
+		return str(obj)
+
+class Token(E3DBBase):
+	__tablename__='token'
+	id=Column(String(64),primary_key=True) # sha1.update(uuid) as token id
+	tenant_id=Column(String(64),ForeignKey('tenant.id'))
+	expired_at=Column(DateTime(),nullable=False)
+	
+	def __repr__(self):
+		obj=dict()
+		obj['id']=self.id
+		obj['tenant_id']=self.tenant_id
+		obj['expired_at']=self.expired_at
+		return str(obj)
+
+def register_role(role_name,desc=''):
+	session=E3DBSession()
+	try:
+		session.begin()
+		role=Role()
+		role.id=str(uuid4())
+		role.name=role_name
+		role.description=desc
+		session.add(role)
+		session.commit()
+	except:
+		session.rollback()
+		return False
+	finally:
+		session.close()
+	return True
+def get_roles():
+	roles=list()
+	session=E3DBSession()
+	try:
+		session.begin()
+		roles=session.query(Role).all()
+	except:
+		pass
+	finally:
+		session.close()
+	return roles;
+def get_role_by_name(role_name):
+	role=None
+	session=E3DBSession()
+	try:
+		session.begin()
+		role=session.query(Role).filter(Role.name==role_name).first()
+	except:
+		pass
+	finally:
+		session.close()
+	return role
+def get_role_by_id(id):
+	role=None
+	session=E3DBSession()
+	try:
+		session.begin()
+		role=session.query(Role).filter(Role.id==id).first()
+	except:
+		pass
+	finally:
+		session.close()
+	return role
+
+
+def get_role_id_by_name(role_name):
+	role_id=None
+	role=get_role_by_name(role_name)
+	if role:
+		role_id=role.id
+	return role_id
+def unregister_role(role_name):
+	role=get_role_by_name(role_name)
+	if role:
+		try:
+			session=E3DBSession()
+			session.begin()
+			session.delete(role)
+			session.commit()
+		except:
+			session.rollback()
+		finally:
+			session.close()
+
+def register_tenant(username,passwd,role_id=None,desc=''):
+	session=E3DBSession()
+	try:
+		session.begin()
+		tenant=Tenant()
+		tenant.id=str(uuid4())
+		tenant.name=username
+		tenant.passwd=passwd
+		tenant.description=desc
+		tenant.enabled=False
+		tenant.role_id=role_id
+		if not role_id:
+			tenant.role_id=get_role_id_by_name('member')
+		session.add(tenant)
+		session.commit()
+	except:
+		session.rollback()
+		return False
+	finally:
+		session.close()
+	return True
+
+def find_tenant_by_name(username):
+	tenant=None
+	session=E3DBSession()
+	try:
+		session.begin()
+		tenant=session.query(Tenant).filter(Tenant.name==username).first()
+	except:
+		tenant=None
+	finally:
+		session.close()
+	return tenant
+def get_tenant_by_id(id):
+	tenant=None
+	session=E3DBSession()
+	try:
+		session.begin()
+		tenant=session.query(Tenant).filter(Tenant.id==id).first()
+	except:
+		tenant=None
+	finally:
+		session.close()
+	return tenant
+def get_tenants():
+	lst=list()
+	session=E3DBSession()
+	try:
+		lst=session.query(Tenant).all()
+	except:
+		lst=list()
+	finally:
+		session.close()
+	return lst
+def _change_tenant_status(id,status):
+	session=E3DBSession()
+	try:
+		session.begin()
+		tenant=session.query(Tenant).filter(Tenant.id==id).first()
+		if tenant:
+			tenant.enabled=status
+			session.commit()
+	except:
+		session.rollback()
+		return False
+	finally:
+		session.close()
+	return True
+def enable_tenant(id):
+	return _change_tenant_status(id,True)
+
+def disable_tenant(id):
+	return _change_tenant_status(id,False)
+
+def unregister_tenant(id):
+	session=E3DBSession()
+	try:
+		session.begin()
+		tenant=session.query(Tenant).filter(Tenant.id==id).first()
+		if tenant :
+			session.delete(tenant)
+			session.commit()
+	except:
+		session.rollback()
+		return False
+	finally:
+		session.close()
+	return True
+	
+if __name__=='__main__':
+	init_e3_database('mysql+pymysql://e3net:e3credientials@localhost/E3common',True)
+	#create_database_entries()
+	#print register_role('member')
+	#print register_role('admin',desc='administrator role')
+	#print register_role('__member__')
+	#print get_role_id_by_name('admin')
+	#unregister_role('admin')
+	#unregister_role('member')
+	#print register_tenant('missant@unitedstack.com','181218z23',desc='my beloved',role_id=get_role_id_by_name('admin'))
+	#print find_tenant_by_name('jzheng')
+	#print get_role_by_id('78eeab15-3118-4fac-aa3d-2ab6f8a920d0')
+	#print get_role_by_id('c6bf5a3b-6159-4167-a6b7-91499a74ec2a')
+	#print get_tenant_by_id('98761cbc-0c6f-40ce-a274-2b896f5809ad')
+	#print disable_tenant('98761cbc-0c6f-40ce-a274-2b896f5809ad')
+	print unregister_tenant('447c2cb9-eb7e-4f25-b821-5ffa503b87c8')
