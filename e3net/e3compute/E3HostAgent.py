bc61a397289b9d5bf0a21ed84beb40a055845b39
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-01 20:05:02 +0000

    Adding tarfile member sanitization to extractall()

--- e3net/e3compute/E3HostAgent.py
+++ e3net/e3compute/E3HostAgent.py
@@ -159,7 +159,26 @@ def extract_image(image_name,path):
     local_file='%s/%s'%(image_host_dir,image_name)
     try:
         with tarfile.open(local_file) as tar:
-            tar.extractall(path=path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, path=path)
         return True
     except:
         return False
