d925e98e10cd37bf2ffa607d171edd166ee75410
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 06:58:28 +0000

    Adding numeric_owner as keyword arguement

--- e3net/e3compute/E3HostAgent.py
+++ e3net/e3compute/E3HostAgent.py
@@ -175,7 +175,7 @@ def extract_image(image_name,path):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=path)
